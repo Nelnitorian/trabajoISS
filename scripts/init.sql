be5af49ab2da87779bce3e0e0810945d88c236fb
Nelnitorian <jujoroal@gmail.com>
2023-11-19 09:15:44 +0100

    añadido nuevo insert

--- scripts/init.sql
+++ scripts/init.sql
@@ -11,3 +11,4 @@ CREATE TABLE incidencias (
 
 INSERT INTO incidencias (id, nombre, numero_patin, causa, fecha_apertura, fecha_cierre) 
 VALUES (1, 'paco', 12, 'Fallo técnico', '2023-01-01 12:12:12', '2023-01-01 23:23:23');
+VALUES (2, 'pepe', 13, 'Robo', '2023-01-02 13:13:13', '2023-01-03 22:22:22');
